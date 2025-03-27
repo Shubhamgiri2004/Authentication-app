49e7a9757bb22d10445910e1b9c0f95dbdf47e12
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-03-27 05:35:10 +0530

    Routes Implemented

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,63 +2,56 @@ import { Request, Response } from "express";
 import User, { IUser } from "../models/userModel";
 import { generateToken } from "../utils/jwtUtils";
 
-
-// writing a interface 
 interface RegisterUserRequest {
-    username: string,
-    email: string,
-    password: string
+    username: string;
+    email: string;
+    password: string;
 }
 
-const registerUser = async (req: Request<RegisterUserRequest>, res: Response) => {
+// User Registration
+export const registerUser = async (req: Request<{}, {}, RegisterUserRequest>, res: Response): Promise<void> => {
     try {
         const { username, email, password } = req.body;
 
-        //to check if userExists
         const userExists = await User.findOne({ email });
-        if (!userExists) return res.status(400).json({
-            message: "User is already exist"
-        })
+        if (userExists) {
+            res.status(400).json({ message: "User already exists" });
+            return;
+        }
 
-        const newUser: IUser = await User.create({ username, email, password });
+        const newUser = await User.create({ username, email, password }) as IUser;
         if (newUser) {
             res.status(201).json({
                 _id: newUser._id,
                 username: newUser.username,
                 email: newUser.email,
                 message: "User created successfully"
-            })
+            });
         } else {
-            res.status(400).json({
-                message: "User can't created"
-            })
+            res.status(400).json({ message: "User could not be created" });
         }
     } catch (error) {
-        res.status(500).json({
-            message: (error as Error).message
-        });
+        res.status(500).json({ message: (error as Error).message });
     }
-}
+};
 
-// user login 
-
-const loginUse = async (req: Request, res:Response) =>{
-    try{
+// User Login
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+    try {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
-        if(user && (await user.comparePassword(password))) {
+        if (user && (await user.comparePassword(password))) {
             res.json({
                 _id: user.id,
                 username: user.username,
                 email: user.email,
                 token: generateToken(user._id as string),
-            })
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password" });
         }
-        
-    } catch( error ) {
-        res.status(500).json({
-            message: (error as Error).message
-        });
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
     }
-}
+};
